1cb0d394fc030dc07d4426c11ea3ff508b71bb04
denco23 <denissobczyk@outlook.com>
2024-03-09 19:11:46 +0100

    problem resolved

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from dotenv import load_dotenv
+from utils import *
+import uuid
+from langchain_community.llms import OpenAI
+
+print("ATTENTION!!!!\nHello World, it actually works, ignore the unncessary Error Message")
